35db072fbef2f707cceaf7b9efb63a2113299184
dnjssla94 <dnjssla94@gmail.com>
2020-09-26 01:22:23 +0900

    그리디 정답제출.

--- codeUp_3321_greedy.py
+++ codeUp_3321_greedy.py
@@ -1,55 +1,26 @@
 
-change = int(input())
+topingNum = int(input())
+douPrice, topingPrice = input().split()
+douPrice = int(douPrice)
+topingPrice = int(topingPrice)
+douCal = int(input())
+topingCal = list()
+calPerDollor = list()
 
-omanwon = 0
-manwon = 0
-ochunwon = 0
-chunwon = 0
-obeak = 0
-beak = 0
-oship = 0
-ship = 0
+for i in range(topingNum):
+    topingCal.append(int(input())) 
 
-while change >= 50000:
-    change -= 50000
-    omanwon +=1
+sorted_topingCal = sorted(topingCal, reverse=True)
+# print(sorted_topingCal)
 
-while change >= 10000:
-    change -= 10000
-    manwon +=1
+for i in range(topingNum + 1):
+    topingCalSum = 0
+    for j in range(i):
+        topingCalSum += sorted_topingCal[j]
+    calPerDollor.append((douCal+topingCalSum)/(douPrice+topingPrice*i))
 
-while change >= 5000:
-    change -= 5000
-    ochunwon +=1
+sorted_calPerDollor = sorted(calPerDollor)
 
-while change >= 1000:
-    change -= 1000
-    chunwon +=1
+# print(sorted_calPerDollor)
+print(int(sorted_calPerDollor[-1]))
 
-while change >= 500:
-    change -= 500
-    obeak +=1
-
-while change >= 100:
-    change -= 100
-    beak +=1
-
-while change >= 50:
-    change -= 50
-    oship +=1
-
-while change >= 10:
-    change -= 10
-    ship +=1
-
-count = omanwon + manwon +ochunwon+chunwon+obeak+beak+oship+ship
-print(count)
-
-print("omanwon: ", omanwon)
-print("manwon: ", manwon)
-print("ochunwon: ", ochunwon)
-print("chunwon: ", chunwon)
-print("obeak: ", obeak)
-print("obeak: ", beak)
-print("oship: ", oship)
-print("ship: ", ship)
